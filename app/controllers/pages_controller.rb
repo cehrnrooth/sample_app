7ec40c769040954f83397a9a84ad5cd265b9b6ef
Carl Ehrnrooth <carl.ehrnrooth@gmail.com>
2012-03-11 13:58:50 -0700

    refactored code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,23 @@
 class PagesController < ApplicationController
+  
   def home
+    @base_title = "Ruby on Rails Tutorial Sample App | "
     @title = "Home"
   end
 
   def contact
+    @base_title = "Ruby on Rails Tutorial Sample App | "
     @title = "Contact"
   end
   
   def about
+    @base_title = "Ruby on Rails Tutorial Sample App | "
     @title = "About"
   end
+  
+  def help
+    @base_title = "Ruby on Rails Tutorial Sample App | "
+    @title = "Help"
+  end
+  
 end
