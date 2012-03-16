21e2bd8c353fa9c8f1b7e8b82d7c2028c5058ff6
Carl Ehrnrooth <carl.ehrnrooth@gmail.com>
2012-03-16 16:28:15 -0700

    finished layout + routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,22 +1,18 @@
 class PagesController < ApplicationController
   
   def home
-    @base_title = "Ruby on Rails Tutorial Sample App | "
     @title = "Home"
   end
 
   def contact
-    @base_title = "Ruby on Rails Tutorial Sample App | "
     @title = "Contact"
   end
   
   def about
-    @base_title = "Ruby on Rails Tutorial Sample App | "
     @title = "About"
   end
   
   def help
-    @base_title = "Ruby on Rails Tutorial Sample App | "
     @title = "Help"
   end
   
